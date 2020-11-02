b3497cda560eba17d4a12bb9128554da2ebf4b31
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-11-02 17:12:57 -0300

    fix: Create Session Validator

--- api/src/modules/users/infra/http/routes/sessions.routes.ts
+++ api/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -6,6 +6,6 @@ import CreateSessionValidator from '../validators/CreateSessionValidator';
 const sessionsRouter = Router();
 const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', CreateSessionValidator, sessionsController.create);
+sessionsRouter.post('/', CreateSessionValidator(), sessionsController.create);
 
 export default sessionsRouter;
