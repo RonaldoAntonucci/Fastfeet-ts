198b1a21e0c973a16975fdb63e090c37624fdc15
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-11-06 15:43:04 -0300

    tests: List Deliveryman deliveries

--- api/__tests__/modules/deliveries/ListDeliverymanDeliveries.e2e-spec.ts
+++ api/__tests__/modules/deliveries/ListDeliverymanDeliveries.e2e-spec.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import 'express-async-errors';
 
 import request from 'supertest';
+import { v4 } from 'uuid';
 
 import { DeliveriesRouter } from '@modules/deliveries';
 import Deliveryman from '@modules/deliveries/infra/typeorm/entities/Deliveryman';
 import FakeDeliverymanAttrs from '@modules/deliveries/models/fakes/FakeDeliverymanAttrs';
 import Delivery from '@modules/deliveries/infra/typeorm/entities/Delivery';
 import FakeDeliveryAttrs from '@modules/deliveries/models/fakes/FakeDeliveryAttrs';
+
 import TestApp from '../../util/TestApp';
 import createToken from '../../util/createToken';
 import Factory, { IFactory } from '../../util/Factory';
@@ -50,6 +52,18 @@ describe('List Deliveryman Deliveries - e2e', () => {
 
     expect(response.status).toBe(200);
     expect(response.body.results.length).toBe(deliveries.length);
+  });
+
+  it('should not be able to list Deliveries if deliveryman not exists.', async () => {
+    token = createToken({ subject: v4(), role: 'deliveryman' });
+
+    const response = await request(app.http())
+      .get('/me')
+      .set('Authorization', token);
+
+    expect(response.status).toBe(400);
+    expect(response.body.status).toBe('error');
+    expect(response.body.message).toBe('Invalid deliveryman id.');
   });
 
   afterEach(async () => {
